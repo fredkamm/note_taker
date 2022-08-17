4ab3fdeb7474035fbed111328aa5280400d2b482
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-08-17 14:01:25 -0400

    initial heroku push

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
-
-const PORT = 3001;
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+const api = require('./routes/notes.js');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 // GET request
-app.get('', (req, res) => {
-    // Let the client know that their request was received
-    res.json(`${req.method} request received`);
-    console.info(`${req.method} request received`);
-  });
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/db', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+);
   
   // POST request
   app.post('', (req, res) => {
