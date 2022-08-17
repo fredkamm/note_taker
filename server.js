e5d6d38dea8cfd69d1edfea1fb14d33dc4afad9f
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-08-17 14:07:30 -0400

    second heroku attempt

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3001;
-const api = require('./routes/notes.js');
+const api = require('./public/notes.html');
 
 const app = express();
 app.use(express.json());
