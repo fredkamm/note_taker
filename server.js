43142f026a397047ef5e556cf8033411df98e379
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-08-17 18:46:17 -0400

    currently, user input is sent to the json file and placed into the array of objects, just need the lists to appear on the page

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const { info } = require("console");
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
@@ -13,7 +12,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-// GET request
+// GET request(s)
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
@@ -23,62 +22,59 @@ app.get('/notes', (req, res) =>
 );
 
 app.get('/api/notes', (req, res) => {
+  // Send a message to the client
+  res.status(200).json(`${req.method} request received to get reviews`);
 
-  res.json(`${req.method} request received to get reviews`);
+  // Log our request to the terminal
+  console.info(`${req.method} Note recieved`);
+});
 
-  console.info(`${req.method} request recieved for notes`);
 
-})
 
 // POST request to add a review
 app.post('/api/notes', (req, res) => {
   // Log that a POST request was received
   console.info(`${req.method} request received to add a review`);
-
   // Destructuring assignment for the items in req.body
-  const { title, text} = req.body;
-
+  const { title, text } = req.body;
   // If all the required properties are present
   if (title && text) {
     // Variable for the object we will save
-    const newReview = {
+    const newNote = {
       title,
       text,
       // note_id: uuid(),
     };
-
     // Obtain existing reviews
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
       if (err) {
         console.error(err);
       } else {
         // Convert string into JSON object
-        const parsedReviews = JSON.parse(data);
+        const parsedNotes = JSON.parse(data);
 
         // Add a new review
-        parsedReviews.push(newReview);
+        parsedNotes.push(newNote);
 
         // Write updated reviews back to the file
         fs.writeFile(
           './db/db.json',
-          JSON.stringify(parsedReviews, null, 4),
+          JSON.stringify(parsedNotes, null, 4),
           (writeErr) =>
             writeErr
               ? console.error(writeErr)
-              : console.info('Successfully updated reviews!')
+              : console.info('Successfully updated note!')
         );
       }
     });
 
     const response = {
       status: 'success',
-      body: newReview,
+      body: newNote,
     };
-
-    console.log(response);
-    res.status(201).json(response);
+    res.json(response);
   } else {
-    res.status(500).json('Error in posting review');
+    res.json('Error in posting feedback');
   }
 });
 
