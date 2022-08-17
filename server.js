a3410609a483c28b7726a31113bab26962c77615
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-08-17 16:46:54 -0400

    fixed links, im able to submit a note but i doesnt not append to page and all notes arent on page yet

--- server.js
+++ server.js
@@ -1,29 +1,94 @@
-const express = require('express');
+const { info } = require("console");
+const express = require("express");
+const fs = require("fs");
 const path = require('path');
-const PORT = process.env.PORT || 3001;
-const api = require('./public/notes.html');
+
 
 const app = express();
-app.use(express.json());
+const PORT = process.env.PORT || 3001;
+
+
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use(express.json());
+app.use(express.static('public'));
+
 
 // GET request
 app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.get('/db', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
+app.get('/api/notes', (req, res) => {
+
+  res.json(`${req.method} request received to get reviews`);
+
+  console.info(`${req.method} request recieved for notes`);
+
+})
+
+// POST request to add a review
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a review`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text} = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newReview = {
+      title,
+      text,
+      // note_id: uuid(),
+    };
+
+    // Obtain existing reviews
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedReviews = JSON.parse(data);
+
+        // Add a new review
+        parsedReviews.push(newReview);
+
+        // Write updated reviews back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedReviews, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully updated reviews!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newReview,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in posting review');
+  }
+});
+
+// Setup listener
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
-  
-  // POST request
-  app.post('', (req, res) => {
-    // Let the client know that their POST request was received
-    res.json(`${req.method} request received`);
-    console.info(`${req.method} request received`);
-  });
-  
-  app.listen(PORT, () =>
-    console.log(`Express server listening on port ${PORT}!`)
-  );
+
+
+
+
+
+
