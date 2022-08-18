a3a62e8bcfeb5fe14362aa2a6b4f3bf3177efc55
fredkamm <105788241+fredkamm@users.noreply.github.com>
2022-08-18 15:35:18 -0400

    app is working officially, need to update readme and possibly add the delete feature

--- server.js
+++ server.js
@@ -13,28 +13,28 @@ app.use(express.static('public'));
 
 
 // GET request(s)
+// reguesting the index.html for the home page
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// giving the notes.html a page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
-  // Send a message to the client
-  res.status(200).json(`${req.method} request received to get reviews`);
-
-  // Log our request to the terminal
-  console.info(`${req.method} Note recieved`);
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    var notes = JSON.parse(data);
+    res.json(notes);
+  });
 });
-
-
 
 // POST request to add a review
 app.post('/api/notes', (req, res) => {
   // Log that a POST request was received
-  console.info(`${req.method} request received to add a review`);
+  console.info(`${req.method} request received to add a note`);
   // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
   // If all the required properties are present
@@ -58,8 +58,7 @@ app.post('/api/notes', (req, res) => {
 
         // Write updated reviews back to the file
         fs.writeFile(
-          './db/db.json',
-          JSON.stringify(parsedNotes, null, 4),
+          './db/db.json', JSON.stringify(parsedNotes, null, 4),
           (writeErr) =>
             writeErr
               ? console.error(writeErr)
